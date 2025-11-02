15bc08f04159c4a1a84eae44703f63bab96acf40
Jayden Lee <jayden04lee@gmail.com>
2025-11-02 13:45:19 -0500

    test wrong test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	t.Run("should return api key", func(t *testing.T) {
+		headers := map[string][]string{
+			"Authorization": {"ApiKey 1234567890"},
+		}
+		apiKey, err := GetAPIKey(headers)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if apiKey != "12345678901" {
+			t.Errorf("expected api key to be 1234567890, got %s", apiKey)
+		}
+	})
+}
